0a05dd07214b3e8b19635c19d6a399629aded859
Plegue <plegue@mail.es>
2016-02-17 10:32:08 +0100

    add sum function

--- new/Adios.java
+++ new/Adios.java
@@ -1,6 +1,11 @@
 public class Adios {
 	
-	private static String nombre = "Name";
+	private static String nombre = "Bye Negin";
+	
+	public static int Sum(int a, int b){
+		int ret = a + b;
+		return ret;
+	}
 	
 	public static void main(String[] zz){
 
